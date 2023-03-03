8efa9f2cc923fc587fede1f94f9a144156113367
Juan Baca Paunero <wencesj@gmail.com>
2023-03-03 10:59:36 -0300

    Initial commit
    This commit adds the application structure and the initial Spring Boot
    dependencies.

---
+++ src/main/java/com/mindata/hotelsearches/HotelSearchesApplication.java
@@ -0,0 +1,13 @@
+package com.mindata.hotelsearches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSearchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelSearchesApplication.class, args);
+    }
+
+}
